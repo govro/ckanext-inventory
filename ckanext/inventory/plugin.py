329fd325cdd3c8246830531d3301b54e45b10254
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-10-28 22:11:58 +0200

    Add inventory_organization_id field to organizations #9

--- ckanext/inventory/plugin.py
+++ ckanext/inventory/plugin.py
@@ -2,9 +2,10 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class InventoryPlugin(plugins.SingletonPlugin):
+class InventoryPlugin(plugins.SingletonPlugin, toolkit.DefaultGroupForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IGroupForm, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
@@ -18,3 +19,39 @@ class InventoryPlugin(plugins.SingletonPlugin):
         mapping.connect('/inventory', controller=controller, action='index')
 
         return mapping
+
+    # IGroupForm
+    def _modify_group_schema(self, schema):
+        schema.update({
+            'inventory_organization_id': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_group_schema(self):
+        schema = super(InventoryPlugin, self).create_group_schema()
+        schema = self._modify_group_schema(schema)
+        return schema
+
+    def update_group_schema(self):
+        schema = super(InventoryPlugin, self).update_group_schema()
+        schema = self._modify_group_schema(schema)
+        return schema
+
+    def show_group_schema(self):
+        schema = super(InventoryPlugin, self).show_group_schema()
+        schema.update({
+            'inventory_organization_id': [
+                tk.get_converter('convert_from_extras'),
+                tk.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        """Register as default handler for groups."""
+        return True
+
+    def group_types(self):
+        """This should handle only organizations, not other types of groups."""
+        return ['organization']
