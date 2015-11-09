2996c015b540c4bd05a968b8ebb5fed6eaad29f0
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-11-09 21:36:40 +0200

    Implement inventory_organization_id directly in the schema #9

--- ckanext/inventory/plugin.py
+++ ckanext/inventory/plugin.py
@@ -1,17 +1,21 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+import ckan.logic.schema
+from ckan.plugins import (implements, IConfigurer, IGroupForm, IRoutes,
+    SingletonPlugin)
+from ckan.plugins.toolkit import (add_template_directory,
+    add_public_directory, add_resource, DefaultOrganizationForm,
+    get_validator, get_converter)
 
 
-class InventoryPlugin(plugins.SingletonPlugin, toolkit.DefaultGroupForm):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IRoutes, inherit=True)
-    plugins.implements(plugins.IGroupForm, inherit=True)
+class InventoryPlugin(SingletonPlugin, DefaultOrganizationForm):
+    implements(IGroupForm, inherit=True)
+    implements(IConfigurer)
+    implements(IRoutes, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'inventory')
+        add_template_directory(config_, 'templates')
+        add_public_directory(config_, 'public')
+        add_resource('fanstatic', 'inventory')
 
     # IRoutes
     def after_map(self, mapping):
@@ -21,38 +25,28 @@ class InventoryPlugin(plugins.SingletonPlugin, toolkit.DefaultGroupForm):
         return mapping
 
     # IGroupForm
-    def _modify_group_schema(self, schema):
+    def form_to_db_schema(self):
+        schema = super(InventoryPlugin, self).form_to_db_schema()
         schema.update({
             'inventory_organization_id': [
-                toolkit.get_validator('ignore_missing'),
-                toolkit.get_converter('convert_to_extras')]
+                get_validator('ignore_missing'),
+                get_converter('convert_to_extras')]
         })
         return schema
 
-    def create_group_schema(self):
-        schema = super(InventoryPlugin, self).create_group_schema()
-        schema = self._modify_group_schema(schema)
-        return schema
-
-    def update_group_schema(self):
-        schema = super(InventoryPlugin, self).update_group_schema()
-        schema = self._modify_group_schema(schema)
-        return schema
-
-    def show_group_schema(self):
-        schema = super(InventoryPlugin, self).show_group_schema()
+    def db_to_form_schema(self):
+        schema = ckan.logic.schema.default_show_group_schema()
         schema.update({
             'inventory_organization_id': [
-                tk.get_converter('convert_from_extras'),
-                tk.get_validator('ignore_missing')]
+                get_validator('ignore_missing'),
+                get_converter('convert_from_extras')]
         })
         return schema
 
     def is_fallback(self):
         """Register as default handler for groups."""
-        return True
+        return False
 
     def group_types(self):
         """This should handle only organizations, not other types of groups."""
-        # TODO @palcu: check why putting organizations here is not working
-        return []
+        return ['organization']
