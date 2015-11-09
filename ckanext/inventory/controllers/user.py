e1dceb5f5556c3cc5c1b245bd1a120c002edaf02
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-11-10 00:49:57 +0200

    Set user in pending after registration #9

--- ckanext/inventory/controllers/user.py
+++ ckanext/inventory/controllers/user.py
@@ -46,20 +46,13 @@ class InventoryUserController(UserController):
             errors = e.error_dict
             error_summary = e.error_summary
             return self.new(data_dict, errors, error_summary)
-        if not c.user:
-            # log the user in programatically
-            rememberer = request.environ['repoze.who.plugins']['friendlyform']
-            identity = {'repoze.who.userid': data_dict['name']}
-            response.headerlist += rememberer.remember(request.environ,
-                                                       identity)
-            h.redirect_to(controller='user', action='me', __ckan_no_root=True)
-        else:
-            # #1799 User has managed to register whilst logged in - warn user
-            # they are not re-logged in as new user.
-            h.flash_success(_('User "%s" is now registered but you are still '
-                            'logged in as "%s" from before') %
-                            (data_dict['name'], c.user))
-            return render('user/logout_first.html')
+
+        model_user = model.Session.query(model.User) \
+                          .filter_by(email=user['email']).first()
+        model_user.set_pending()
+
+        h.flash_success('Your account registration will be reviewed.')
+        return render('home/index.html')
 
     def _add_user_to_organization(self, model, user, inventory_organization_id):
         rev = model.repo.new_revision()
