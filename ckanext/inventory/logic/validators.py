964cb658d02af62270b181a74aea53fdd2354b5d
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-11-30 19:13:25 +0200

    Add error message when inventory entry is not provided #5

--- ckanext/inventory/logic/validators.py
+++ ckanext/inventory/logic/validators.py
@@ -6,6 +6,9 @@ def update_package_inventory_entry(value, context):
     model = context['model']
     session = context['session']
 
+    if not value:
+        raise Invalid(_('No inventory entry provided'))
+
     result = session.query(InventoryEntry).filter_by(id=value).first()
 
     if not result:
