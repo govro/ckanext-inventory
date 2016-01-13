029a382cb1b70de10b52b0a9599b6d378ad216ff
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-14 00:27:40 +0200

    Add hardcoded entries for the public table #6

--- ckanext/inventory/controllers/inventory.py
+++ ckanext/inventory/controllers/inventory.py
@@ -1,5 +1,12 @@
-from ckan.plugins.toolkit import BaseController, render
+from ckan.plugins.toolkit import BaseController, render, c
 
 class InventoryController(BaseController):
     def index(self):
+        entries = [{
+            'name': 'datagov',
+            'id': 'datagov',
+            'ontime_entries': 3,
+            'late_entries': 2
+        }]
+        c.entries = entries
         return render('inventory/index.html')
